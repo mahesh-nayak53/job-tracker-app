58180f9632e953a31f140041916b3cfd5d167988
mahesh-nayak53 <maheshnayakgvt06@gmail.com>
2026-06-02 09:06:25 +0530

    updated ai url

--- backend/src/main/java/com/jobtracker/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/jobtracker/backend/config/SecurityConfig.java
@@ -76,7 +76,7 @@ public class SecurityConfig {
 
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
-                "https://job-tracker-app-olive.vercel.app"
+                "https://job-tracker-app-olive.vercel.app/"
         ));
 
         config.setAllowedMethods(List.of(
