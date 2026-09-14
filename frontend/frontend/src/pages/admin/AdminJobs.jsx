8a890c1bbc69f817cd4b3b05aae5a1f8f6aae961
mahesh-nayak53 <maheshnayakgvt06@gmail.com>
2026-09-15 01:10:44 +0530

    Fix AdminJobs API import

--- frontend/frontend/src/pages/admin/AdminJobs.jsx
+++ frontend/frontend/src/pages/admin/AdminJobs.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import API from "../api/api";
+import API from "../../api/axios";
 
 export default function AdminJobs() {
   const [jobs, setJobs] = useState([]);
